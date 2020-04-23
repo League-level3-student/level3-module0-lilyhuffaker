afddc1546e9b6639e0c016d0c9a483ae6e0c459c
missl <missl@LAPTOP-UNQ0FKIG>
2020-04-23 16:28:37 -0700

    you finished visual array sorter and started array methods

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,19 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		double sum = sumIntArray(values);
+		double average = sum/values.length;
+		return average;
 	}
 	
 	
@@ -20,8 +24,14 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
+		for(int i = 0; i < array.length; i++) {
+			if(value == array[i]) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
 	}
 	
 	//4. Complete the method so that it returns the index of the,
